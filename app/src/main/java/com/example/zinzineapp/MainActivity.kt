8470d033841e08ea9f369decb6eb999fd607fbd7
EloiseMcGlasson <mc.glasson@free.fr>
2024-08-27 12:14:56 +0200

    first commit

---
+++ app/src/main/java/com/example/zinzineapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.zinzineapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.ComponentActivity
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+                    setContentView(R.layout.activity_main)
+                    // Passage à la page des émissions en cliquant sur le bouton
+        val button= findViewById<Button>(R.id.open_emissions)
+        button.setOnClickListener {
+            finish()
+            startActivity(Intent(applicationContext, Emissions::class.java))
+        }
+                }
+            }
